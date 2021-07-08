5c9063312e4c9908c7eeaadfa9d87127c4ae5d5d
Jugnu Agrawal <jugnuagrawal.brjn@gmail.com>
2021-07-08 23:36:52 +0530

    fixed sns-confirmation

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const router = require('express').Router();
 const got = require('got');
 
-router.post(async (req, res, next) => {
-    try {
-        let payload = req.body;
-        if (typeof payload === 'string') {
-            payload = JSON.parse(payload);
+module.exports = function (options = { parseMessage: false }) {
+    return async (req, res, next) => {
+        try {
+            let payload = req.body;
+            if (typeof payload === 'string') {
+                payload = JSON.parse(payload);
+            }
+            req.body = payload;
+            if (req.header('x-amz-sns-message-type') === 'SubscriptionConfirmation') {
+                await got(payload.SubscribeURL);
+                return res.status(200).end();
+            }
+            if (options.parseMessage && req.header('x-amz-sns-message-type') === 'Notification') {
+                let data = payload.Message;
+                if(typeof data === 'string'){
+                    data = JSON.parse(data);
+                }
+                req.body = data;
+            }
+            next();
+        } catch (e) {
+            if (typeof e === 'string') {
+                throw new Error(e);
+            }
+            throw e;
         }
-        req.body = payload;
-        if (req.header('x-amz-sns-message-type') === 'SubscriptionConfirmation') {
-            await got(payload.SubscribeURL);
-            return res.status(200).end();
-        }
-        //  else if (req.header('x-amz-sns-message-type') === 'Notification') {
-
-        // }
-        next();
-    } catch (e) {
-        if (typeof e === 'string') {
-            throw new Error(e);
-        }
-        throw e;
-    }
-});
-
-module.exports = function () {
-    return router;
+    };
 };
